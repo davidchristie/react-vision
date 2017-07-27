100cc7e6249f9d2034806e9f83b5b56013752e50
David Christie <davidchristie.nz@gmail.com>
2017-07-27 13:09:45 +1200

    Create application

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import './App.css'
+
+export default class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <h1>React Vision</h1>
+      </div>
+    )
+  }
+}
